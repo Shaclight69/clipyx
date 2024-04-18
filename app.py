25ab07b6f5924b8827b9251d148b321046ba2495
Shaclight69 <dhiaghorbel0@gmail.com>
2024-04-18 20:02:07 +0100

    feat: pytube video downloader

---
+++ app.py
@@ -0,0 +1,80 @@
+import customtkinter as ctk
+from tkinter import ttk
+from pytube import YouTube
+import os 
+
+def donwload_video():
+    url = entry_url.get()
+    resolution = resolution_var.get()
+
+    progress_label.pack(pady=(10, 5))
+    progress_bar.pack(pady=(10, 5))
+    status_label.pack(pady=(10, 5))
+
+    try:
+       yt = YouTube(url, on_progress_callback=on_progress)
+       stream = yt.streams.filter(res=resolution).first()
+       os.path.join("downloads", f"{yt.title}.mp4")
+       stream.download(output_path="downloads")
+
+       status_label.configure(text="Successfully downloaded", text_color="white", fg_color="green")
+
+    except Exception as e:
+       status_label.configure(text=f"Error {str(e)}", text_color="white", fg_color="red")
+
+
+def on_progress(stream, chunk, bytes_remaining):
+   total_size = stream.fileize
+   bytes_downloaded = total_size - bytes_remaining
+   percentage_completed = bytes_downloaded / total_size * 100
+
+   progress_label.configure(text = str(int(percentage_completed)) + "%")
+   progress_label.update()
+   
+   progress_bar.set(float(percentage_completed / 100))
+
+
+# create a root window
+root = ctk.CTk()
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("green")
+# title of the window
+root.title("Clipyx 🎞️")
+
+# set min/max width and height
+root.geometry("720x480")
+root.minsize(720,480)
+root.maxsize(1080,720)
+
+#content frame
+content_frame= ctk.CTkFrame(root)
+content_frame.pack(fill=ctk.BOTH, expand=True, padx=10, pady=10)
+
+#video url entry widget
+url_label= ctk.CTkLabel(content_frame, text="Enter the youtube url here : ")
+entry_url= ctk.CTkEntry(content_frame, width=400, height=40)
+url_label.pack(pady=(10, 5))
+entry_url.pack(pady=(10, 5))
+
+#download button
+download_button= ctk.CTkButton(content_frame, text="Download", command=donwload_video)
+download_button.pack(pady=(10, 5))
+
+#resolutions combo-box
+resolutions=["1080p","720p","360p","240p"]
+resolution_var = ctk.StringVar()
+resolution_combobox= ttk.Combobox(content_frame, values=resolutions,textvariable=resolution_var)
+resolution_combobox.pack(pady=(10, 5))
+resolution_combobox.set("1080p")
+
+#download progress bar
+progress_label = ctk.CTkLabel(content_frame, text="0%")
+
+progress_bar = ctk.CTkProgressBar(content_frame, width=400)
+progress_bar.set(0.6)
+
+status_label = ctk.CTkLabel(content_frame, text="Downloaded")
+
+
+# to start the app
+root.mainloop()
